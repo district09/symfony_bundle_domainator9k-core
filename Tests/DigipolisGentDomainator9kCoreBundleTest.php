d6fe9b9c0a36858652c1f8408590f6f710adc497
Pieter MAssoels <massoelspieter@gmail.com>
2018-01-03 14:11:28 +0100

    Basic test added

---
+++ Tests/DigipolisGentDomainator9kCoreBundleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DigipolisGent\Domainator9k\CoreBundle\Tests;
+
+use DigipolisGent\Domainator9k\CoreBundle\DependencyInjection\Compiler\ApplicationTypePass;
+use DigipolisGent\Domainator9k\CoreBundle\DependencyInjection\Compiler\CiTypePass;
+use DigipolisGent\Domainator9k\CoreBundle\DependencyInjection\Compiler\TaskPass;
+use DigipolisGent\Domainator9k\CoreBundle\DigipolisGentDomainator9kCoreBundle;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Description of DigipolisGentDomainator9kCoreBundleTest.
+ *
+ * @author Jelle Sebreghts
+ */
+class DigipolisGentDomainator9kCoreBundleTest extends TestCase
+{
+    public function testBuild()
+    {
+        $bundle = new DigipolisGentDomainator9kCoreBundle();
+        $container = $this->getMockBuilder(ContainerBuilder::class)->disableOriginalConstructor()->getMock();
+        $bundle->build($container);
+    }
+}

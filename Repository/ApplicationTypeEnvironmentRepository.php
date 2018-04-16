c16b560d6d82608d5113a951d1454e6df8223903
Matthijs Van Assche <matthijs@fanatict.be>
2018-04-16 10:20:34 +0200

    Fixed application type join.

--- Repository/ApplicationTypeEnvironmentRepository.php
+++ Repository/ApplicationTypeEnvironmentRepository.php
@@ -6,12 +6,21 @@ use Doctrine\ORM\EntityRepository;
 
 class ApplicationTypeEnvironmentRepository extends EntityRepository
 {
+    /**
+     * Get all environments of the specified application type.
+     *
+     * @param $type
+     *   The application type name.
+     *
+     * @return array
+     *   The query results.
+     */
     public function findAllByApplicationType($type) {
         return $this->createQueryBuilder('ate')
-            ->innerJoin('ate.applicationType', 'at', Query\Expr\Join::ON)
+            ->innerJoin('ate.applicationType', 'at')
             ->andWhere('at.name = :name')
             ->setParameter('name', $type)
             ->getQuery()
-            ->execute();
+            ->getResult();
     }
 }

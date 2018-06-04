d96afd2fd06b63b4c7e6227a2de6f4e81aa1b26d
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2018-06-04 19:51:11 +0200

    Fix indentation.

--- Twig/TemplateHelpExtension.php
+++ Twig/TemplateHelpExtension.php
@@ -9,51 +9,52 @@ use Twig\Error\RuntimeError;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
-class TemplateHelpExtension extends AbstractExtension {
+class TemplateHelpExtension extends AbstractExtension
+{
 
-  protected $tokenService;
+    protected $tokenService;
 
-  public function __construct(TokenService $tokenService) {
-      $this->tokenService = $tokenService;
-  }
+    public function __construct(TokenService $tokenService) {
+        $this->tokenService = $tokenService;
+    }
 
-  public function getFunctions()
-  {
-      return [
-          new TwigFunction(
-              'template_help',
-              [
-                  $this,
-                  'templateHelp',
-              ],
-              [
-                  'needs_environment' => true,
-                  'is_safe' => [
-                      'html',
-                  ],
-              ]
-          ),
-      ];
-  }
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction(
+                'template_help',
+                [
+                    $this,
+                    'templateHelp',
+                ],
+                [
+                    'needs_environment' => true,
+                    'is_safe' => [
+                        'html',
+                    ],
+                ]
+            ),
+        ];
+    }
 
-  public function templateHelp(Environment $environment, array $classes, $textarea)
-  {
+    public function templateHelp(Environment $environment, array $classes, $textarea)
+    {
 
-      $templates = [
-          'token' => array_keys($this->tokenService->getTemplateReplacements()),
-      ];
-      foreach ($classes as $key => $class) {
-          if (!is_a($class, TemplateInterface::class, true)) {
-              new RuntimeError(sprintf('Class %s does not implement %s.', $class, TemplateInterface::class));
-          }
-          $templates[$key] = array_keys(call_user_func([$class, 'getTemplateReplacements']));
-      }
+        $templates = [
+            'token' => array_keys($this->tokenService->getTemplateReplacements()),
+        ];
+        foreach ($classes as $key => $class) {
+            if (!is_a($class, TemplateInterface::class, true)) {
+                new RuntimeError(sprintf('Class %s does not implement %s.', $class, TemplateInterface::class));
+            }
+            $templates[$key] = array_keys(call_user_func([$class, 'getTemplateReplacements']));
+        }
 
-      return $environment->render(
-          '@DigipolisGentDomainator9kCore/Template/templatehelper.twig',
-          [
-              'templates' => $templates,
-              'textarea' => $textarea,
-          ]);
-  }
+        return $environment->render(
+            '@DigipolisGentDomainator9kCore/Template/templatehelper.twig',
+            [
+                'templates' => $templates,
+                'textarea' => $textarea,
+            ]);
+    }
 }

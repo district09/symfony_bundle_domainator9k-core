8c14f62c1d1dbd276a20e9fb350a45839f1b78dd
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2021-02-18 16:30:02 +0100

    Document a deprecation we didn't fix yet
    Most of the explanation is in the comment, but we documented a
    deprecation we didn't fix, because the fix we want to use is not
    available yet in Symfony 3.4, only in Symfony 4.

--- Command/AbstractCommand.php
+++ Command/AbstractCommand.php
@@ -16,7 +16,14 @@ abstract class AbstractCommand extends ContainerAwareCommand
      */
     protected function runNextTask(string $type)
     {
-        $this->getContainer()
+        $this->getContainer
+            // @deprecated
+            // This is deprecated in Symfony 4, but we leave it here
+            // intentionally so we'll _have_ to fix it once we upgrade from 3 to
+            // 4. The only way to do it in 3.4 is to make the service public,
+            // which we don't want. Symfony 4 supports injecting service
+            // dependencies in commands in the constructor (allowing them to be
+            // private, Symfony 3 doesn't.
             ->get(TaskRunnerService::class)
             ->runNext($type);
     }

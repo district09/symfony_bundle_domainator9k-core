257d37e3feb978e7d43b4b37fb0b98818d869d03
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-09-18 16:22:16 +0200

    Add support for https repository urls
    Some instances might need to clone over https and not SSH.

--- Entity/AbstractApplication.php
+++ Entity/AbstractApplication.php
@@ -48,8 +48,8 @@ abstract class AbstractApplication implements TemplateInterface
      *
      * @ORM\Column(name="git_repo", type="string", nullable=false)
      * @Assert\Regex(
-     *     pattern="/^git@[a-z0-9]*.[a-z]*:[a-zA-Z0-9]*\/[^\s.\\]*.git$/",
-     *     message="The git repository url must be in the git@xxx.git format"
+     *     pattern="/^(git@[a-z0-9]*\.[a-z]*:|https:\/\/[a-z0-9]*\.[a-z]*\/)[a-zA-Z0-9]*\/[^\s.\\]*\.git$/",
+     *     message="The git repository url must be in the git@host:respository.git or the https://host/repository.git format"
      * )
      */
     protected $gitRepo;

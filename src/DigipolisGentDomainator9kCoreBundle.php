8712178ae187d79c58197932e470cac2c195c19c
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-12-06 15:23:22 +0100

    Move methods form the extension class to the bundle class

--- src/DigipolisGentDomainator9kCoreBundle.php
+++ src/DigipolisGentDomainator9kCoreBundle.php
@@ -5,14 +5,21 @@ namespace DigipolisGent\Domainator9k\CoreBundle;
 use DigipolisGent\Domainator9k\CoreBundle\DependencyInjection\Compiler\CacheClearProviderCompilerPass;
 use DigipolisGent\Domainator9k\CoreBundle\DependencyInjection\Compiler\CliFactoryProviderCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class DigipolisGentDomainator9kCoreBundle extends Bundle
+class DigipolisGentDomainator9kCoreBundle extends AbstractBundle
 {
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
         $container->addCompilerPass(new CacheClearProviderCompilerPass());
         $container->addCompilerPass(new CliFactoryProviderCompilerPass());
+    }
+
+    #[\Override]
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yml');
     }
 }
